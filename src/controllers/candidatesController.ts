43595345892bdc1bc3667909d93c3e9fe6be4471
rickutino <rickutino@gmail.com>
2022-05-16 23:26:24 +0900

    add candidates Routes,Controller and database

---
+++ src/controllers/candidatesController.ts
@@ -0,0 +1,106 @@
+import { Request, Response } from 'express'
+import { Candidate } from '../models/candidate'
+
+const candidatesController = {
+  index: async (req: Request, res: Response) => {
+    try {
+      const candidates = await Candidate.findAll()
+      return res.json(candidates)
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
+  },
+
+  save: async (req: Request, res: Response) => {
+    const { name, bio, email, phone, openToWork } = req.body
+    console.log(name, bio, email, phone, openToWork)
+
+
+    try {
+      const candidate = await Candidate.create({
+        name,
+        bio,
+        email,
+        phone,
+        openToWork
+      })
+      console.log(candidate)
+
+      return res.status(201).json(candidate)
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
+  },
+
+  show: async (req: Request, res: Response) => {
+    const { id } = req.params
+
+    try {
+      const candidate = await Candidate.findByPk(id)
+
+      if (candidate === null) { 
+        return res.status(404).json({ message: 'Candidato não encontrado' })
+      }
+
+      return res.json(candidate)
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
+  },
+
+  update: async (req: Request, res: Response) => {
+    const { id } = req.params
+    const { name, bio, email, phone, openToWork } = req.body
+
+    try {
+      const candidate = await Candidate.findByPk(id)
+
+      if (candidate === null) {
+        return res.status(404).json({ message: 'Candidato não encontrado' })
+      }
+
+      candidate.name = name
+      candidate.bio = bio
+      candidate.email = email
+      candidate.phone = phone
+      candidate.openToWork = openToWork
+
+      await candidate.save()
+
+      return res.status(200).json(candidate)
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
+  },
+
+  delete: async (req: Request, res: Response) => {
+    const { id } = req.params
+
+
+    try {
+        const userDeleted = await Candidate.destroy({
+            where: { id: id }
+        })
+
+      if (userDeleted === 0) {
+        return res.status(404).json({ message: 'Candidato não encontrado' })
+      }
+
+        return res.status(204).send()
+    } catch (err) {
+        if (err instanceof Error) {
+            return res.status(400).json({ message: err.message })
+        }
+    }
+}
+}
+
+export { candidatesController }

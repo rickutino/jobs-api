15e91243eb56205e98d5f05487e6340ef244acb0
rickutino <rickutino@gmail.com>
2022-05-17 22:41:19 +0900

    refac: refactoring model import

--- src/controllers/candidatesController.ts
+++ src/controllers/candidatesController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express'
-import { Candidate } from '../models/candidate'
+import { Candidate } from '../models'
 
 const candidatesController = {
   index: async (req: Request, res: Response) => {

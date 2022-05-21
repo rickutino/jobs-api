f925ceda5c11b7a7b96149b7646753715e83b5f4
rickutino <rickutino@gmail.com>
2022-05-21 19:43:28 +0900

    refac: refactoring job Model, add JobCreationAttribute interface

--- src/models/job.ts
+++ src/models/job.ts
@@ -1,50 +1,72 @@
-import { sequelize } from '../database'
-import { BelongsToManyAddAssociationMixin, BelongsToManyCountAssociationsMixin, BelongsToManyRemoveAssociationMixin, DataTypes, Model } from 'sequelize'
+import {
+  BelongsToManyAddAssociationMixin,
+  BelongsToManyAddAssociationsMixin,
+  BelongsToManyCountAssociationsMixin,
+  BelongsToManyGetAssociationsMixin,
+  BelongsToManyHasAssociationMixin,
+  BelongsToManyRemoveAssociationMixin,
+  DataTypes,
+  Model,
+  Optional,
+  Sequelize
+} from 'sequelize'
+
 import { CandidateInstance } from './candidate'
 
-interface JobInstance extends Model {
-    id: number
-    title: string
-    description: string
-    limitDate: Date
-    companyId: number
-    addCandidate: BelongsToManyAddAssociationMixin<CandidateInstance, number>
-    removeCandidate: BelongsToManyRemoveAssociationMixin<CandidateInstance, number>
-    countCandidates: BelongsToManyCountAssociationsMixin
+export interface Job {
+  id: number
+  title: string
+  description: string
+  limitDate: Date
+  companyId: number
 }
 
-const Job = sequelize.define<JobInstance>(
-  'jobs',
-  {
+export interface JobCreatingAttribute extends Optional<Job, 'id'> {}
+
+export interface JobInstance extends Model<Job, JobCreatingAttribute>, Job {
+  addCandidate: BelongsToManyAddAssociationMixin<CandidateInstance, number>
+  addCandidates: BelongsToManyAddAssociationsMixin<CandidateInstance, number>
+  removeCandidate: BelongsToManyRemoveAssociationMixin<CandidateInstance, number>
+  countCandidates: BelongsToManyCountAssociationsMixin
+  getCandidates: BelongsToManyGetAssociationsMixin<CandidateInstance>
+  hasCandidate: BelongsToManyHasAssociationMixin<CandidateInstance, number>
+}
+
+export default (sequelize: Sequelize) => {
+  const Job = sequelize.define<JobInstance, Job>(
+    'jobs',
+    {
       id: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-          autoIncrement: true,
-          primaryKey: true
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
       },
       title: {
-          type: DataTypes.STRING,
-          allowNull: false
+        type: DataTypes.STRING,
+        allowNull: false
       },
       description: {
-          type: DataTypes.TEXT,
-          allowNull: false
+        type: DataTypes.TEXT,
+        allowNull: false
       },
       limitDate: {
-          type: DataTypes.DATE,
-          allowNull: false
+        type: DataTypes.DATE,
+        allowNull: false
       },
       companyId: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-          references: {
-              model: 'companies',
-              key: 'id'
-          },
-          onUpdate: 'CASCADE',
-          onDelete: 'RESTRICT'
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'companies',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'RESTRICT'
       }
-  }
-)
+    }
+  )
 
-export { Job }
+  return Job 
+}
+

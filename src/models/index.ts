6dc1c3401f2e94743cf284ab33e8c1bb880b963d
rickutino <rickutino@gmail.com>
2022-05-21 19:46:58 +0900

    refac: refactoring import model

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,13 @@
-import { Candidate } from './candidate'
-import { Company } from './company'
-import { Job } from './job'
+import { connect } from '../database';
+import candidate from './candidate';
+import company from './company';
+import job from './job';
+
+const sequelize = connect()
+
+const Candidate = candidate(sequelize)
+const Company = company(sequelize)
+const Job = job(sequelize)
 
 Candidate.belongsToMany(Job, { through: 'job_candidates'})
 
@@ -10,7 +17,9 @@ Job.belongsTo(Company)
 Job.belongsToMany(Candidate, { through: 'job_candidates'})
 
 export {
+    sequelize,
     Candidate,
     Company,
     Job
-}
+};
+

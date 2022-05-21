c263e9ca5ab79e2f9b79cf77cfe97c66d90b0472
rickutino <rickutino@gmail.com>
2022-05-21 22:10:36 +0900

    chore: add db test config

--- config/sequelize-cli.js
+++ config/sequelize-cli.js
@@ -1,6 +1,11 @@
 require('dotenv').config();
 
 module.exports = {
-    dialect: 'postgres',
+  dialect: 'postgres',
+  development: {
     url: process.env.DATABASE_URL
+  },
+  test: {
+    url: process.env.DATABASE_URL?.replace(/_development/, '_test')
+  }
 }
